f469421c5f209bf1930b6f973724c3736a7ec54a
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2022-02-27 15:35:34 +0600

    Pre-Loader Done

--- resources/views/BangladeshAdmin/Jobposts/VacancyApproval.blade.php
+++ resources/views/BangladeshAdmin/Jobposts/VacancyApproval.blade.php
@@ -10,6 +10,9 @@
     <link href="{{ asset('assets/plugins/datatables/responsive.bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/plugins/datatables/scroller.bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/approval_vacancy.css') }}" rel="stylesheet" type="text/css" />
+
+
 @endsection
 
 @section('main-content')
@@ -66,9 +69,9 @@
                                             <td>
                                                 @if ($appliedVacancy->status == 'Applied')
                                                     <span class="badge badge-info">Applied</span>
-                                                @elseif($appliedVacancy->status == "Rejected")
+                                                @elseif($appliedVacancy->status == 'Rejected')
                                                     <span class="badge badge-danger">Rejected</span>
-                                                @elseif($appliedVacancy->status == "Approved")
+                                                @elseif($appliedVacancy->status == 'Approved')
                                                     <span class="badge badge-success">Approved</span>
                                                 @else
                                                     <span class="badge badge-info">{{ $appliedVacancy->status }}</span>
@@ -85,12 +88,11 @@
                                                         <i class="fa fa-edit"></i>
                                                     </a>
 
-                                                    <button type="button"
-                                                        class="btn btn-danger btn-xs" onclick="reject(this)"
-                                                        value="{{ route('BangladeshAdmin.jobPost.rejectVacancy', $appliedVacancy->id) }}" >
+                                                    <button type="button" class="btn btn-danger btn-xs"
+                                                        onclick="reject(this)"
+                                                        value="{{ route('BangladeshAdmin.jobPost.rejectVacancy', $appliedVacancy->id) }}">
                                                         <i class="fa fa-times" aria-hidden="true"></i>
                                                     </button>
-
                                                 @else
                                                     <a class="btn btn-info btn-xs"
                                                         href="{{ route('BangladeshAdmin.jobPost.viewVacancy', $appliedVacancy->id) }}">
@@ -114,8 +116,210 @@
                                         <th>Status</th>
                                         <th>Action</th>
                                     </tr>
+
                                 </tfoot>
+
                             </table>
+                            <div class='text-center'>
+                                
+                                <button id="button" type="button" class="btn btn-info">Click Here</button>
+
+                                <div id="overlay">
+      <div class="cv-spinner">
+        <span class="spinner">
+          <svg id="wrap" width="300" height="300">
+            <!-- background -->
+            <svg>
+              <circle
+                cx="150"
+                cy="150"
+                r="130"
+                style="stroke: lightblue; stroke-width: 18; fill: transparent;"
+              />
+              <circle cx="150" cy="150" r="115" style="fill: #2c3e50;" />
+              <path
+                style="
+                  stroke: #2c3e50;
+                  stroke-dasharray: 820;
+                  stroke-dashoffset: 820;
+                  stroke-width: 18;
+                  fill: transparent;
+                "
+                d="M150,150 m0,-130 a 130,130 0 0,1 0,260 a 130,130 0 0,1 0,-260"
+              >
+                <animate
+                  attributeName="stroke-dashoffset"
+                  dur="6s"
+                  to="-820"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </svg>
+
+            <!-- image -->
+            <svg>
+              <path
+                id="hourglass"
+                d="M150,150 C60,85 240,85 150,150 C60,215 240,215 150,150 Z"
+                style="stroke: white; stroke-width: 5; fill: white;"
+              />
+
+              <path
+                id="frame"
+                d="M100,97 L200, 97 M100,203 L200,203 M110,97 L110,142 M110,158 L110,200 M190,97 L190,142 M190,158 L190,200 M110,150 L110,150 M190,150 L190,150"
+                style="
+                  stroke: lightblue;
+                  stroke-width: 6;
+                  stroke-linecap: round;
+                "
+              />
+
+              <animateTransform
+                xlink:href="#frame"
+                attributeName="transform"
+                type="rotate"
+                begin="0s"
+                dur="3s"
+                values="0 150 150; 0 150 150; 180 150 150"
+                keyTimes="0; 0.8; 1"
+                repeatCount="indefinite"
+              />
+              <animateTransform
+                xlink:href="#hourglass"
+                attributeName="transform"
+                type="rotate"
+                begin="0s"
+                dur="3s"
+                values="0 150 150; 0 150 150; 180 150 150"
+                keyTimes="0; 0.8; 1"
+                repeatCount="indefinite"
+              />
+            </svg>
+
+            <!-- sand -->
+            <svg>
+              <!-- upper part -->
+              <polygon
+                id="upper"
+                points="120,125 180,125 150,147"
+                style="fill: #2c3e50;"
+              >
+                <animate
+                  attributeName="points"
+                  dur="3s"
+                  keyTimes="0; 0.8; 1"
+                  values="120,125 180,125 150,147; 150,150 150,150 150,150; 150,150 150,150 150,150"
+                  repeatCount="indefinite"
+                />
+              </polygon>
+
+              <!-- falling sand -->
+              <path
+                id="line"
+                stroke-linecap="round"
+                stroke-dasharray="1,4"
+                stroke-dashoffset="200.00"
+                stroke="#2c3e50"
+                stroke-width="2"
+                d="M150,150 L150,198"
+              >
+                <!-- running sand -->
+                <animate
+                  attributeName="stroke-dashoffset"
+                  dur="3s"
+                  to="1.00"
+                  repeatCount="indefinite"
+                />
+                <!-- emptied upper -->
+                <animate
+                  attributeName="d"
+                  dur="3s"
+                  to="M150,195 L150,195"
+                  values="M150,150 L150,198; M150,150 L150,198; M150,198 L150,198; M150,195 L150,195"
+                  keyTimes="0; 0.65; 0.9; 1"
+                  repeatCount="indefinite"
+                />
+                <!-- last drop -->
+                <animate
+                  attributeName="stroke"
+                  dur="3s"
+                  keyTimes="0; 0.65; 0.8; 1"
+                  values="#2c3e50;#2c3e50;transparent;transparent"
+                  to="transparent"
+                  repeatCount="indefinite"
+                />
+              </path>
+
+              <!-- lower part -->
+              <g id="lower">
+                <path
+                  d="M150,180 L180,190 A28,10 0 1,1 120,190 L150,180 Z"
+                  style="stroke: transparent; stroke-width: 5; fill: #2c3e50;"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    type="translate"
+                    keyTimes="0; 0.65; 1"
+                    values="0 15; 0 0; 0 0"
+                    dur="3s"
+                    repeatCount="indefinite"
+                  />
+                </path>
+                <animateTransform
+                  xlink:href="#lower"
+                  attributeName="transform"
+                  type="rotate"
+                  begin="0s"
+                  dur="3s"
+                  values="0 150 150; 0 150 150; 180 150 150"
+                  keyTimes="0; 0.8; 1"
+                  repeatCount="indefinite"
+                />
+              </g>
+
+              <!-- lower overlay - hourglass -->
+              <path
+                d="M150,150 C60,85 240,85 150,150 C60,215 240,215 150,150 Z"
+                style="stroke: white; stroke-width: 5; fill: transparent;"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  begin="0s"
+                  dur="3s"
+                  values="0 150 150; 0 150 150; 180 150 150"
+                  keyTimes="0; 0.8; 1"
+                  repeatCount="indefinite"
+                />
+              </path>
+
+              <!-- lower overlay - frame -->
+              <path
+                id="frame"
+                d="M100,97 L200, 97 M100,203 L200,203"
+                style="
+                  stroke: lightblue;
+                  stroke-width: 6;
+                  stroke-linecap: round;
+                "
+              >
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  begin="0s"
+                  dur="3s"
+                  values="0 150 150; 0 150 150; 180 150 150"
+                  keyTimes="0; 0.8; 1"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </svg>
+          </svg>
+        </span>
+      </div>
+    </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -168,6 +372,30 @@
             })
         }
     </script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+<script>
+         jQuery(function ($) {
+        $(document).ajaxSend(function () {
+          $("#overlay").fadeIn(300);
+        });
+
+        $("#button").click(function () {
+          $.ajax({
+            type: "GET",
+            success: function (data) {
+              console.log(data);
+            }
+          }).done(function () {
+            setTimeout(function () {
+              $("#overlay").fadeOut(300);
+            }, 1200);
+          });
+        });
+      });
+    </script>
+
 @endsection
 
 
