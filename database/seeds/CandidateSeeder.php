fc29e8f25b4d450a2ddbbc403d6eaa5cc782cc9f
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-03-05 18:50:00 +0600

    candidate seeder updated

--- database/seeds/CandidateSeeder.php
+++ database/seeds/CandidateSeeder.php
@@ -89,26 +89,27 @@ class CandidateSeeder extends Seeder
 
 
         for ($i=0; $i <800 ; $i++) {
-            DB::table('candidates')->insert([
-                'candidate_name' =>  $faker->name(),
-                'role_id' => 15,
-                'job_id' => rand(1, 10),
-                'company_id' => rand(1, 13),
-                'job_category_id' => rand(1, 13),
-                'candidate_dob' => '2021-30-07',
-                'candidate_gender' => 'male',
-                'passport_number' => Str::random(18),
-                'phone_number' => '0185623055'.$i,
-                'candidate_email' => 'candidate' . $i . '@gmail.com',
-                'status' => 'Active',
-                'offered_status' => true,
-                // 'result_status' => 'New',
-                'nationality' => 'bangladesh',
-                'present_address' => 'dhaka bangladesh',
-                'permanent_address' => 'bahrain',
-                'created_id' => 3,
-                'created_at' => Carbon::now(),
-            ]);
+            $candidate = new Candidate();
+            $candidate->candidate_name      =  $faker->name();
+            $candidate->role_id             = 15;
+            $candidate->job_id              = $faker->numberBetween(1, 25);
+            $candidate->company_id          = rand(18, 27);
+            $candidate->job_category_id     = rand(1, 13);
+            $candidate->candidate_dob       = $faker->dateTimeBetween($startDate = '-40 years', $endDate = '-20 years', $timezone = null);
+            $candidate->candidate_gender    = $faker->randomElements(['male', 'female'])[0];
+            $candidate->passport_number     = Str::random(18);
+            $candidate->phone_number        = $faker->phoneNumber();
+            $candidate->candidate_email     = $faker->safeEmail;
+            $candidate->status              = 'Active';
+            $candidate->offered_status      = $faker->randomElements([true, false])[0];
+            $candidate->result_status       = 'New';
+            $candidate->nationality         = 'bangladesh';
+            $candidate->present_address     = 'dhaka bangladesh';
+            $candidate->permanent_address   = 'dhaka bangladesh';
+            $candidate->created_id          = $faker->numberBetween(28,37);
+            $candidate->permission_status   = $faker->numberBetween(1,2);
+            $candidate->created_at          = Carbon::now();
+            $candidate->save();
         }
     }
 }
